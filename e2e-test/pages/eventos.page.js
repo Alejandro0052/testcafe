71ca27d1cc3499d9159be68ae61a614f52a8ccf6
Alejandro <mariaeugenialopez456@gmail.com>
2025-07-14 23:21:12 -0500

    Agregando back a eventos, despues que fue solucionado el error desde el frontend del aplicativo al cual se le estan haciendo las pruebas

--- e2e-test/pages/eventos.page.js
+++ e2e-test/pages/eventos.page.js
@@ -3,16 +3,16 @@ import { Selector } from 'testcafe';
 class EventosPage {
   constructor() {
     this.card = Selector('#cardeventos');
-    //this.backButton = Selector('#backeventos');
+    this.backButton = Selector('#backeventos');
   }
 
   async validatePage(t) {
     await t.expect(this.card.exists).ok();
   }
 
-//   async goBack(t) {
-//     await t.click(this.backButton);
-//   }
+   async goBack(t) {
+     await t.click(this.backButton);
+   }
 }
 
 export default new EventosPage();
